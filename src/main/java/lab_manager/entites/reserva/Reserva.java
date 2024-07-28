7e9b31667ab506fdb92c45210f8da44f8f340f43
João Pedro <jp.siq.snts@gmail.com>
2024-07-28 14:58:48 -0300

    fix: adicao de campos restantes na reserva

--- src/main/java/lab_manager/entites/reserva/Reserva.java
+++ src/main/java/lab_manager/entites/reserva/Reserva.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +15,10 @@ import lombok.NoArgsConstructor;
 public class Reserva<T extends Reservavel> {
 	private T item;
 	private Usuario usuario;
+	private LocalDateTime saidaPrevista;
+	private LocalDateTime devolucaoPrevista;
+	private LocalDateTime saida;
+	private LocalDateTime devolucao;
+	private LocalDateTime cancelamento;
+	private EReservavel reservavel;
 }
